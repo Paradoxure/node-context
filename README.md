d639fd0bbf4d857c1bed4ed14912eee904c0534d
Leonardo Lobeto <llobeto@despegar.com>
2019-04-11 00:28:22 -0300

    Created

---
+++ README.md
@@ -0,0 +1,2 @@
+# zorzal2-context
+Por ahora, solo un wrapper sobre [express-http-context](https://github.com/skonves/express-http-context#readme "Documentación en GitHub")
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as context from 'express-http-context';
+
+export const get = context.get;
+export const set = context.set;
+export const middleware = context.middleware;
